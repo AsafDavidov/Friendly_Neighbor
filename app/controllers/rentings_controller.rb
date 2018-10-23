397f80fcc0a264726a3bc078c5eb647c369627dc
kristopherbullinger <github email address>
2018-10-23 09:20:00 -0400

    Update Rentings

--- app/controllers/rentings_controller.rb
+++ app/controllers/rentings_controller.rb
@@ -1,8 +1,34 @@
 class RentingsController < ApplicationController
 
   def new
+    find_item
+    @renting = @item.rentings.build
+  end
+
+  def create
+    byebug
+    find_item
+    @renting = @item.rentings.build(renting_params)
+    @renting.user = get_user
+    if @renting.save
+      redirect_to renting_path(@renting)
+    else
+      flash[:errors] = @renting.errors.full_messages
+      redirect_to new_renting_path
+    end
+  end
+
+  private
+  def renting_params
+    params.require(:renting).permit(:item_id, :proposed_duration)
+  end
+
+  def find_item
     @item = Item.find(params[:item_id])
-    @renting = Renting.new
+  end
+
+  def get_user
+    User.find(session[:user_id])
   end
 
 end
